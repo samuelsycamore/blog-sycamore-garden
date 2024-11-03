0c9d2676fbffaaaa448c60b33086de2376ff1c6b
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2024-11-03 07:41:31 -0800

    initializing

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "Sycamore Garden";
+export const SITE_DESCRIPTION = "Thoughts on technical writing and software engineering.";
